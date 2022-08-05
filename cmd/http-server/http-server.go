3ca34868b51140d6351a413d1ef04f9aacbf1d1a
Thom Shutt <thomshutt@users.noreply.github.com>
2022-08-05 20:51:38 +0100

    Use correct values for this project in Mist Config JSON (#10)
    * Use correct values for this project in Mist Config JSON
    
    * Not a connector
    
    * Proper description

--- cmd/http-server/http-server.go
+++ cmd/http-server/http-server.go
@@ -19,7 +19,7 @@ func main() {
 	flag.Parse()
 
 	if *mistJson {
-		mistconnector.PrintMistConfigJson("mist-api-connector", "Sidecar for connecting Mist with Catalyst API", "Mist API Connector", config.Version, flag.CommandLine)
+		mistconnector.PrintMistConfigJson("catalyst-api", "HTTP API server for translating Catalyst API requests into Mist calls", "Catalyst API", config.Version, flag.CommandLine)
 		return
 	}
 
