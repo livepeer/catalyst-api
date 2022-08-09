0d957e4ed284e85072a0fbbedc03755cf39c17c9
Thom Shutt <thomshutt@users.noreply.github.com>
2022-08-09 14:38:11 +0100

    Add HTTP callback client for communicating status to Studio

---
+++ clients/callback_client.go
@@ -0,0 +1,111 @@
+package clients
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+const max_retries = 3
+const backoff_millis = 200
+
+type CallbackClient struct {
+	httpClient *http.Client
+}
+
+func NewCallbackClient() CallbackClient {
+	return CallbackClient{
+		httpClient: &http.Client{},
+	}
+}
+
+func (c CallbackClient) DoWithRetries(r *http.Request) error {
+	var resp *http.Response
+	var err error
+	for x := 0; x < max_retries; x++ {
+		resp, err = c.httpClient.Do(r)
+		if err == nil && resp.StatusCode == http.StatusOK {
+			return nil
+		}
+
+		// Back off to give us more chance of succeeding during a network blip
+		time.Sleep(backoff_millis * time.Millisecond)
+	}
+
+	if err != nil {
+		return fmt.Errorf("failed to send callback to %q. Error: %q", r.URL.String(), err)
+	}
+	return fmt.Errorf("failed to send callback to %q. Response Status Code: %d", r.URL.String(), resp.StatusCode)
+}
+
+func (c CallbackClient) SendTranscodeStatus(url string, status TranscodeStatus, completionRatio float32) error {
+	tsm := TranscodeStatusMessage{
+		CompletionRatio: completionRatio,
+		Status:          status.String(),
+	}
+
+	j, err := json.Marshal(tsm)
+	if err != nil {
+		return err
+	}
+
+	r, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(j))
+	if err != nil {
+		return err
+	}
+
+	return c.DoWithRetries(r)
+}
+
+func (c CallbackClient) SendTranscodeStatusError(callbackURL, errorMsg string) error {
+	tsm := TranscodeStatusMessage{
+		Error:  errorMsg,
+		Status: TranscodeStatusError.String(),
+	}
+
+	j, err := json.Marshal(tsm)
+	if err != nil {
+		return err
+	}
+
+	r, err := http.NewRequest(http.MethodPost, callbackURL, bytes.NewReader(j))
+	if err != nil {
+		return err
+	}
+
+	return c.DoWithRetries(r)
+}
+
+// An enum of potential statuses a Transcode job can have
+
+type TranscodeStatus int
+
+const (
+	TranscodeStatusPreparing TranscodeStatus = iota
+	TranscodeStatusTranscoding
+	TranscodeStatusCompleted
+	TranscodeStatusError
+)
+
+type TranscodeStatusMessage struct {
+	CompletionRatio float32 `json:"completion_ratio,omitempty"`
+	Error           string  `json:"error,omitempty"`
+	Retriable       bool    `json:"retriable,omitempty"`
+	Status          string  `json:"status,omitempty"`
+}
+
+func (ts TranscodeStatus) String() string {
+	switch ts {
+	case TranscodeStatusPreparing:
+		return "preparing"
+	case TranscodeStatusTranscoding:
+		return "transcoding"
+	case TranscodeStatusCompleted:
+		return "completed"
+	case TranscodeStatusError:
+		return "error"
+	}
+	return "unknown"
+}
