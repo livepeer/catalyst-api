0c9d2ad298b82af8f017b11051c973f139f54f85
Alex Kordic <alexandarkordic@gmail.com>
2022-11-04 16:35:18 +0100

    Use LogError()

--- handlers/misttriggers/recording_end.go
+++ handlers/misttriggers/recording_end.go
@@ -146,14 +146,14 @@ func (d *MistCallbackHandlersCollection) triggerRecordingEndSegmenting(w http.Re
 			err := createDtsh(requestID, output.Manifest)
 			if err != nil {
 				// should not block the ingestion flow or make it fail on error.
-				log.Log(requestID, "master createDtsh() failed", "code", err, "destination", output.Manifest)
+				log.LogError(requestID, "master createDtsh() failed", err, "destination", output.Manifest)
 			}
 			for _, rendition := range output.Videos {
 				// we create dtsh for all rendition playlists
 				err := createDtsh(requestID, rendition.Location)
 				if err != nil {
 					// should not block the ingestion flow or make it fail on error.
-					log.Log(requestID, "createDtsh() failed", "code", err, "destination", rendition.Location)
+					log.LogError(requestID, "createDtsh() failed", err, "destination", rendition.Location)
 				}
 			}
 		}
@@ -245,7 +245,7 @@ func createDtsh(requestID, destination string) error {
 	}
 	go func() {
 		if err := headerPrepare.Wait(); err != nil {
-			log.Log(requestID, "createDtsh return code", "code", err, "destination", destination)
+			log.LogError(requestID, "createDtsh return code", err, "destination", destination)
 		}
 	}()
 	return nil
