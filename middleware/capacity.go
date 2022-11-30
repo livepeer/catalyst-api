0e3c52e44ce3f135e3e0621c3cae93704471d0a8
Thom Shutt <thomshutt@users.noreply.github.com>
2022-11-30 11:45:54 +0000

    Reduce max jobs in flight

--- middleware/capacity.go
+++ middleware/capacity.go
@@ -7,7 +7,10 @@ import (
 	"github.com/livepeer/catalyst-api/pipeline"
 )
 
-const MAX_JOBS_IN_FLIGHT = 10
+// Somewhat arbitrary and conservative number of maximum Catalyst VOD jobs in the system
+// at one time. We can look at more sophisticated strategies for calculating capacity in
+// the future.
+const MAX_JOBS_IN_FLIGHT = 5
 
 func HasCapacity(vodEngine *pipeline.Coordinator, next httprouter.Handle) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
