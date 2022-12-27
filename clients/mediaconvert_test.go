c73fcffd545c710b5867bfda95c0ba7038c01f51
Victor Elias <victorgelias@gmail.com>
2022-12-27 13:09:14 -0300

    clients: Add test for progress reporting

--- clients/mediaconvert_test.go
+++ clients/mediaconvert_test.go
@@ -7,38 +7,32 @@ import (
 	"os"
 	"path"
 	"testing"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/mediaconvert"
+	"github.com/cenkalti/backoff/v4"
+	"github.com/livepeer/catalyst-api/config"
 	"github.com/stretchr/testify/require"
 )
 
 func TestOnlyS3URLsToAWSClient(t *testing.T) {
 	require := require.New(t)
-	f, err := os.CreateTemp(os.TempDir(), "user-input-*")
-	require.NoError(err)
-	defer os.Remove(f.Name())
-	_, err = f.WriteString(exampleFileContents)
-	require.NoError(err)
-
-	// use the random file name as the dir name for the transfer file
-	mcInputDir := path.Join(os.TempDir(), "out", f.Name())
-	transferredFile := path.Join(mcInputDir, "input/1234/video")
-	defer os.Remove(transferredFile)
-
-	mc := &MediaConvert{
-		s3TransferBucket:    mustParseURL("s3://thebucket"),
-		osTransferBucketURL: mustParseURL("file://" + mcInputDir),
-		client: stubMediaConvertClient{
-			createJob: func(input *mediaconvert.CreateJobInput) (*mediaconvert.CreateJobOutput, error) {
-				// check that only an s3:// URL is sent to AWS client
-				require.Equal("s3://thebucket/input/1234/video", *input.Settings.Inputs[0].FileInput)
-				require.Equal("s3://thebucket/output/1234/index", *input.Settings.OutputGroups[0].OutputGroupSettings.HlsGroupSettings.Destination)
-				return nil, errors.New("secret error")
-			},
+	awsStub := &stubMediaConvertClient{
+		createJob: func(input *mediaconvert.CreateJobInput) (*mediaconvert.CreateJobOutput, error) {
+			// check that only an s3:// URL is sent to AWS client
+			require.Equal("s3://thebucket/input/1234/video", *input.Settings.Inputs[0].FileInput)
+			require.Equal("s3://thebucket/output/1234/index", *input.Settings.OutputGroups[0].OutputGroupSettings.HlsGroupSettings.Destination)
+			return nil, errors.New("secret error")
 		},
 	}
-	err = mc.Transcode(context.Background(), TranscodeJobArgs{
+	mc, f, transferDir, cleanup := setupTestMediaConvert(t, awsStub)
+	defer cleanup()
+
+	transferredFile := path.Join(transferDir, "input/1234/video")
+	defer os.Remove(transferredFile)
+
+	err := mc.Transcode(context.Background(), TranscodeJobArgs{
 		InputFile:     mustParseURL("file://" + f.Name()),
 		HLSOutputFile: mustParseURL("s3+https://endpoint.com/bucket/1234/index.m3u8"),
 		CollectSourceSize: func(size int64) {
@@ -48,15 +42,58 @@ func TestOnlyS3URLsToAWSClient(t *testing.T) {
 	require.ErrorContains(err, "secret error")
 
 	// Check that the file was copied to the osTransferBucketURL folder
-	_, err = os.Stat(transferredFile)
+	content, err := os.ReadFile(transferredFile)
 	require.NoError(err)
+	require.Equal(exampleFileContents, string(content))
+}
+
+func TestReportsMediaConvertProgress(t *testing.T) {
+	require := require.New(t)
+
+	createJobCalls, getJobCalls := 0, 0
+	awsStub := &stubMediaConvertClient{
+		createJob: func(input *mediaconvert.CreateJobInput) (*mediaconvert.CreateJobOutput, error) {
+			createJobCalls++
+			return &mediaconvert.CreateJobOutput{Job: &mediaconvert.Job{Id: aws.String("10")}}, nil
+		},
+		getJob: func(input *mediaconvert.GetJobInput) (*mediaconvert.GetJobOutput, error) {
+			getJobCalls++
+			switch getJobCalls {
+			case 1:
+				return &mediaconvert.GetJobOutput{Job: &mediaconvert.Job{
+					Status:             aws.String(mediaconvert.JobStatusProgressing),
+					JobPercentComplete: aws.Int64(50),
+				}}, nil
+			case 2:
+				return nil, errors.New("done with this test")
+			default:
+				require.Fail("unexpected call")
+				return nil, errors.New("unreachable")
+			}
+		},
+	}
+	mc, f, transferDir, cleanup := setupTestMediaConvert(t, awsStub)
+	defer cleanup()
+	defer os.Remove(path.Join(transferDir, "input/1234/video"))
+
+	reportProgressCalls := 0
+	err := mc.Transcode(context.Background(), TranscodeJobArgs{
+		InputFile:         mustParseURL("file://" + f.Name()),
+		HLSOutputFile:     mustParseURL("s3+https://endpoint.com/bucket/1234/index.m3u8"),
+		CollectSourceSize: func(size int64) {},
+		ReportProgress: func(progress float64) {
+			reportProgressCalls++
+			require.InEpsilon(0.5, progress, 1e-9)
+		},
+	})
+	require.ErrorContains(err, "done with this test")
+	require.Equal(1, createJobCalls)
+	require.Equal(2, getJobCalls)
+	require.Equal(1, reportProgressCalls)
 }
 
 func TestSendsOriginalURLToS3OnCopyError(t *testing.T) {
 	require := require.New(t)
-	mcInputDir := path.Join(os.TempDir(), "out")
-	transferredFile := path.Join(mcInputDir, "input/1234/video")
-	defer os.Remove(transferredFile)
 
 	awsStub := &stubMediaConvertClient{
 		createJob: func(input *mediaconvert.CreateJobInput) (*mediaconvert.CreateJobOutput, error) {
@@ -66,11 +103,9 @@ func TestSendsOriginalURLToS3OnCopyError(t *testing.T) {
 			return nil, errors.New("secret error")
 		},
 	}
-	mc := &MediaConvert{
-		s3TransferBucket:    mustParseURL("s3://thebucket"),
-		osTransferBucketURL: mustParseURL("file://" + mcInputDir),
-		client:              awsStub,
-	}
+	mc, _, transferDir, cleanup := setupTestMediaConvert(t, awsStub)
+	defer cleanup()
+
 	err := mc.Transcode(context.Background(), TranscodeJobArgs{
 		// use a non existing HTTP endpoint for the file
 		InputFile:         mustParseURL("http://localhost:3000/not-here.mp4"),
@@ -85,7 +120,7 @@ func TestSendsOriginalURLToS3OnCopyError(t *testing.T) {
 		return nil, errors.New("unreachable")
 	}
 	err = mc.Transcode(context.Background(), TranscodeJobArgs{
-		// use a non existing an OS URL
+		// use a non existing OS URL
 		InputFile:         mustParseURL("s3+https://user:pwd@localhost:4321/bucket/no-minio-here.mp4"),
 		HLSOutputFile:     mustParseURL("s3+https://endpoint.com/bucket/1234/index.m3u8"),
 		CollectSourceSize: func(size int64) {},
@@ -93,62 +128,86 @@ func TestSendsOriginalURLToS3OnCopyError(t *testing.T) {
 	require.ErrorContains(err, "download error")
 
 	// Check that no file was created to the osTransferBucketURL folder
-	_, err = os.Stat(transferredFile)
+	_, err = os.Stat(path.Join(transferDir, "input/1234/video"))
 	require.ErrorContains(err, "no such file")
 }
 
 func TestRetriesOnAccelerationError(t *testing.T) {
 	require := require.New(t)
-	mcInputDir := path.Join(os.TempDir(), "out")
-	transferredFile := path.Join(mcInputDir, "input/1234/video")
-	defer os.Remove(transferredFile)
 
 	createdJobs := 0
-	mc := &MediaConvert{
-		s3TransferBucket:    mustParseURL("s3://thebucket"),
-		osTransferBucketURL: mustParseURL("file://" + mcInputDir),
-		client: stubMediaConvertClient{
-			createJob: func(input *mediaconvert.CreateJobInput) (*mediaconvert.CreateJobOutput, error) {
-				createdJobs++
-				switch createdJobs {
-				case 1:
-					require.Equal(*input.AccelerationSettings.Mode, mediaconvert.AccelerationModePreferred)
-					return &mediaconvert.CreateJobOutput{Job: &mediaconvert.Job{Id: aws.String("420")}}, nil
-				case 2:
-					require.Nil(input.AccelerationSettings)
-					return nil, errors.New("we are done with this test")
-				default:
-					require.Fail("should not have been called")
-					return nil, errors.New("unreachable")
-				}
-			},
-			getJob: func(input *mediaconvert.GetJobInput) (*mediaconvert.GetJobOutput, error) {
-				switch *input.Id {
-				case "420":
-					return &mediaconvert.GetJobOutput{Job: &mediaconvert.Job{
-						Status:       aws.String(mediaconvert.JobStatusError),
-						ErrorMessage: aws.String("enhance your calm"),
-						ErrorCode:    aws.Int64(1550),
-					}}, nil
-				default:
-					require.Fail("unknown job id " + *input.Id)
-					return nil, errors.New("unreachable")
-				}
-			},
+	awsStub := &stubMediaConvertClient{
+		createJob: func(input *mediaconvert.CreateJobInput) (*mediaconvert.CreateJobOutput, error) {
+			createdJobs++
+			switch createdJobs {
+			case 1:
+				require.Equal(*input.AccelerationSettings.Mode, mediaconvert.AccelerationModePreferred)
+				return &mediaconvert.CreateJobOutput{Job: &mediaconvert.Job{Id: aws.String("420")}}, nil
+			case 2:
+				require.Nil(input.AccelerationSettings)
+				return nil, errors.New("we are done with this test")
+			default:
+				require.Fail("should not have been called")
+				return nil, errors.New("unreachable")
+			}
+		},
+		getJob: func(input *mediaconvert.GetJobInput) (*mediaconvert.GetJobOutput, error) {
+			switch *input.Id {
+			case "420":
+				return &mediaconvert.GetJobOutput{Job: &mediaconvert.Job{
+					Status:       aws.String(mediaconvert.JobStatusError),
+					ErrorMessage: aws.String("enhance your calm"),
+					ErrorCode:    aws.Int64(1550),
+				}}, nil
+			default:
+				require.Fail("unknown job id " + *input.Id)
+				return nil, errors.New("unreachable")
+			}
 		},
 	}
+	mc, inputFile, transferDir, cleanup := setupTestMediaConvert(t, awsStub)
+	defer cleanup()
+	defer os.Remove(path.Join(transferDir, "input/1234/video"))
+
 	err := mc.Transcode(context.Background(), TranscodeJobArgs{
 		// use a non existing HTTP endpoint for the file
-		InputFile:         mustParseURL("http://localhost:3000/not-here.mp4"),
+		InputFile:         mustParseURL("file://" + inputFile.Name()),
 		HLSOutputFile:     mustParseURL("s3+https://endpoint.com/bucket/1234/index.m3u8"),
 		CollectSourceSize: func(size int64) {},
 	})
 	require.ErrorContains(err, "done with this test")
 	require.Equal(2, createdJobs)
+}
 
-	// Check that no file was created to the osTransferBucketURL folder
-	_, err = os.Stat(transferredFile)
-	require.ErrorContains(err, "no such file")
+func setupTestMediaConvert(t *testing.T, awsStub AWSMediaConvertClient) (mc *MediaConvert, inputFile *os.File, transferDir string, cleanup func()) {
+	oldConstantBackoff, oldExponentialBackoff, oldRetries := constantBackOff, exponentialBackOff, config.DownloadOSURLRetries
+	constantBackOff = backoff.NewConstantBackOff(1 * time.Millisecond)
+	exponentialBackOff = backoff.NewExponentialBackOff()
+	exponentialBackOff.InitialInterval = 1 * time.Millisecond
+	exponentialBackOff.MaxInterval = 1 * time.Millisecond
+	config.DownloadOSURLRetries = 1
+
+	var err error
+	inputFile, err = os.CreateTemp(os.TempDir(), "user-input-*")
+	require.NoError(t, err)
+	_, err = inputFile.WriteString(exampleFileContents)
+	require.NoError(t, err)
+	require.NoError(t, inputFile.Close())
+
+	// use the random file name as the dir name for the transfer file
+	transferDir = path.Join(inputFile.Name()+"_dir", "transfer")
+
+	cleanup = func() {
+		constantBackOff, exponentialBackOff, config.DownloadOSURLRetries = oldConstantBackoff, oldExponentialBackoff, oldRetries
+		require.NoError(t, os.Remove(inputFile.Name()))
+	}
+
+	mc = &MediaConvert{
+		s3TransferBucket:    mustParseURL("s3://thebucket"),
+		osTransferBucketURL: mustParseURL("file://" + transferDir),
+		client:              awsStub,
+	}
+	return
 }
 
 func mustParseURL(str string) *url.URL {
@@ -164,14 +223,14 @@ type stubMediaConvertClient struct {
 	getJob    func(*mediaconvert.GetJobInput) (*mediaconvert.GetJobOutput, error)
 }
 
-func (s stubMediaConvertClient) CreateJob(input *mediaconvert.CreateJobInput) (*mediaconvert.CreateJobOutput, error) {
+func (s *stubMediaConvertClient) CreateJob(input *mediaconvert.CreateJobInput) (*mediaconvert.CreateJobOutput, error) {
 	if s.createJob == nil {
 		return nil, errors.New("not implemented")
 	}
 	return s.createJob(input)
 }
 
-func (s stubMediaConvertClient) GetJob(input *mediaconvert.GetJobInput) (*mediaconvert.GetJobOutput, error) {
+func (s *stubMediaConvertClient) GetJob(input *mediaconvert.GetJobInput) (*mediaconvert.GetJobOutput, error) {
 	if s.getJob == nil {
 		return nil, errors.New("not implemented")
 	}
