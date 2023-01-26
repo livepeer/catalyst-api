b3e51d79de464e6815c45b2925d5e8b1ac571402
Max Holland <max@livepeer.org>
2023-01-26 10:12:18 +0000

    review comments

--- video/probe.go
+++ video/probe.go
@@ -8,17 +8,6 @@ import (
 	"gopkg.in/vansante/go-ffprobe.v2"
 )
 
-/*
-type ProbeInfo struct {
-	Format  *ffprobe.Format
-	Streams []*ffprobe.Stream
-}
-
-func (p ProbeInfo) String() string {
-	return fmt.Sprintf("Format: %v \nStreams: %v", p.Format, p.Streams)
-}
-*/
-
 func ProbeFileReader(ctx context.Context, fileReader io.Reader) (*ffprobe.ProbeData, error) {
 	return ffprobe.ProbeReader(ctx, fileReader)
 }
@@ -28,7 +17,6 @@ func ProbeFileFromOS(file io.Reader) (*ffprobe.ProbeData, error) {
 	probeCtx, probeCancel := context.WithTimeout(context.Background(), 60*time.Second)
 	defer probeCancel()
 
-	// TODO: add HTTP probing (via ffprobe.ProbeURL in addition to ffprobe.ProbeReader)
 	data, err := ProbeFileReader(probeCtx, file)
 	if err != nil {
 		return nil, err
