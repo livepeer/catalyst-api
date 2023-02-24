b412d9a2016540a5dfbcdf7f99bcc42950361327
Eli Mallon <eli@livepeer.org>
2023-02-24 14:01:31 -0800

    config: integratic mapic cli parameters

--- config/cli.go
+++ config/cli.go
@@ -2,16 +2,32 @@ package config
 
 import (
 	"flag"
+	"fmt"
 	"net/url"
 	"strings"
+	"time"
 )
 
 type Cli struct {
 	Port                      int
+	Host                      string
+	MistHost                  string
+	MistUser                  string
+	MistPassword              string
 	MistPort                  int
 	MistHttpPort              int
+	MistConnectTimeout        time.Duration
+	MistStreamSource          string
+	MistHardcodedBroadcasters string
+	MistScrapeMetrics         bool
+	MistSendAudio             string
+	MistBaseStreamName        string
+	AMQPURL                   string
+	OwnUri                    string
+	OwnRegion                 string
 	PromPort                  int
 	APIToken                  string
+	APIServer                 string
 	SourceOutput              string
 	PrivateBucketURL          *url.URL
 	ExternalTranscoder        string
@@ -56,4 +72,33 @@ func parseURLs(s string, dest *[]*url.URL) error {
 	}
 	*dest = urls
 	return nil
+}
+
+type InvertedBool struct {
+	Value *bool
+}
+
+func (f *InvertedBool) String() string {
+	return "foo"
+}
+
+func (f *InvertedBool) IsBoolFlag() bool {
+	return true
+}
+
+func (f *InvertedBool) Set(value string) error {
+	if value == "true" {
+		*f.Value = false
+	} else if value == "false" {
+		*f.Value = true
+	} else {
+		return fmt.Errorf("only true and false values allowed")
+	}
+	return nil
+}
+
+// MistController has trouble giving us `-booleanFlag=false` values, so we use `-noBooleanFlag=true` instead 🤷‍♂️
+func InvertedBoolFlag(fs *flag.FlagSet, dest *bool, name string, value bool, usage string) {
+	*dest = value
+	fs.Var(&InvertedBool{dest}, fmt.Sprintf("no-%s", name), usage)
 }
