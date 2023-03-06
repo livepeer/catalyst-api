158c59e843b467467562135704979b02d01d3997
Eli Mallon <eli@livepeer.org>
2023-03-06 08:00:30 +0000

    main: refactor main function, add two http servers

--- api/http.go
+++ api/http.go
@@ -1,14 +1,48 @@
 package api
 
 import (
+	"context"
+	"net/http"
+	"time"
+
 	"github.com/julienschmidt/httprouter"
+	"github.com/livepeer/catalyst-api/config"
 	"github.com/livepeer/catalyst-api/handlers"
 	"github.com/livepeer/catalyst-api/handlers/misttriggers"
+	"github.com/livepeer/catalyst-api/log"
 	"github.com/livepeer/catalyst-api/middleware"
 	"github.com/livepeer/catalyst-api/pipeline"
 )
 
-func AddRoutes(router *httprouter.Router, vodEngine *pipeline.Coordinator, apiToken string) *httprouter.Router {
+func ListenAndServe(ctx context.Context, addr string, apiToken string, vodEngine *pipeline.Coordinator) error {
+	router := NewCatalystAPIRouter(vodEngine, apiToken)
+	server := http.Server{Addr: addr, Handler: router}
+	ctx, cancel := context.WithCancel(ctx)
+
+	log.LogNoRequestID(
+		"Starting Catalyst API!",
+		"version", config.Version,
+		"host", addr,
+	)
+
+	var err error
+	go func() {
+		err = server.ListenAndServe()
+		cancel()
+	}()
+
+	<-ctx.Done()
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	return server.Shutdown(ctx)
+}
+
+func NewCatalystAPIRouter(vodEngine *pipeline.Coordinator, apiToken string) *httprouter.Router {
+	router := httprouter.New()
 	withLogging := middleware.LogRequest()
 	withAuth := middleware.IsAuthorized
 	withCapacityChecking := middleware.HasCapacity
--- mapic/start_mapic.go
+++ mapic/start_mapic.go
@@ -1,70 +1,21 @@
 package mistapiconnector
 
 import (
-	"errors"
-	"net"
-	"time"
-
-	"github.com/golang/glog"
 	"github.com/livepeer/catalyst-api/config"
-	mistapi "github.com/livepeer/catalyst-api/mapic/apis/mist"
-	"github.com/livepeer/catalyst-api/mapic/metrics"
-	"github.com/livepeer/catalyst-api/mapic/model"
-	"github.com/livepeer/go-api-client"
 )
 
-func StartMapic(cli *config.Cli) IMac {
-	lapi, _ := api.NewAPIClientGeolocated(api.ClientOptions{
-		Server:      cli.APIServer,
-		AccessToken: cli.APIToken,
-	})
-
-	mapi := mistapi.NewMist(cli.MistHost, cli.MistUser, cli.MistPassword, cli.APIToken, uint(cli.MistPort))
-	ensureLoggedIn(mapi, cli.MistConnectTimeout)
-	metrics.InitCensus(cli.NodeName, model.Version, "mistconnector")
-
-	opts := MacOptions{
-		NodeID:                    cli.NodeName,
-		MistHost:                  cli.MistHost,
-		MistAPI:                   mapi,
-		LivepeerAPI:               lapi,
-		BaseStreamName:            cli.MistBaseStreamName,
-		CheckBandwidth:            false,
-		SendAudio:                 cli.MistSendAudio,
-		AMQPUrl:                   cli.AMQPURL,
-		OwnRegion:                 cli.OwnRegion,
-		MistStreamSource:          cli.MistStreamSource,
-		MistHardcodedBroadcasters: cli.MistHardcodedBroadcasters,
-		NoMistScrapeMetrics:       !cli.MistScrapeMetrics,
-	}
-	mc, err := NewMac(opts)
-	if err != nil {
-		glog.Fatalf("Error creating mist-api-connector %v", err)
-	}
-	if err := mc.SetupTriggers(cli.OwnInternalURL() + "/mapic"); err != nil {
-		glog.Fatal(err)
+func NewMapic(cli *config.Cli) IMac {
+	mc := &mac{
+		config:                    cli,
+		nodeID:                    cli.NodeName,
+		mistHot:                   cli.MistHost,
+		checkBandwidth:            false,
+		streamInfo:                make(map[string]*streamInfo),
+		baseStreamName:            cli.MistBaseStreamName,
+		srvShutCh:                 make(chan error),
+		ownRegion:                 cli.OwnRegion,
+		mistStreamSource:          cli.MistStreamSource,
+		mistHardcodedBroadcasters: cli.MistHardcodedBroadcasters,
 	}
 	return mc
-}
-
-func ensureLoggedIn(mapi *mistapi.API, timeout time.Duration) {
-	deadline := time.NewTimer(timeout)
-	defer deadline.Stop()
-	for {
-		err := mapi.Login()
-		if err == nil {
-			return
-		}
-
-		var netErr net.Error
-		if !errors.As(err, &netErr) {
-			glog.Fatalf("Fatal non-network error logging to mist. err=%q", err)
-		}
-		select {
-		case <-deadline.C:
-			glog.Fatalf("Failed to login to mist after %s. err=%q", timeout, netErr)
-		case <-time.After(1 * time.Second):
-			glog.Errorf("Retrying after network error logging to mist. err=%q", netErr)
-		}
-	}
 }
