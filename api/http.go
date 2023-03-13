59c109d19e481f8ad7abc448218006823b508462
Eli Mallon <eli@livepeer.org>
2023-03-13 07:43:56 +0000

    handlers: finish refactoring out catalyst-node handlers

--- api/http.go
+++ api/http.go
@@ -6,23 +6,23 @@ import (
 	"time"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/livepeer/catalyst-api/balancer"
+	"github.com/livepeer/catalyst-api/cluster"
 	"github.com/livepeer/catalyst-api/config"
-	"github.com/livepeer/catalyst-api/handlers"
-	"github.com/livepeer/catalyst-api/handlers/misttriggers"
+	"github.com/livepeer/catalyst-api/handlers/geolocation"
 	"github.com/livepeer/catalyst-api/log"
 	"github.com/livepeer/catalyst-api/middleware"
-	"github.com/livepeer/catalyst-api/pipeline"
 )
 
-func ListenAndServe(ctx context.Context, addr string, apiToken string, vodEngine *pipeline.Coordinator) error {
-	router := NewCatalystAPIRouter(vodEngine, apiToken)
-	server := http.Server{Addr: addr, Handler: router}
+func ListenAndServe(ctx context.Context, cli config.Cli, bal balancer.Balancer, c cluster.Cluster) error {
+	router := NewCatalystAPIRouter(cli, bal, c)
+	server := http.Server{Addr: cli.HTTPAddress, Handler: router}
 	ctx, cancel := context.WithCancel(ctx)
 
 	log.LogNoRequestID(
 		"Starting Catalyst API!",
 		"version", config.Version,
-		"host", addr,
+		"host", cli.HTTPAddress,
 	)
 
 	var err error
@@ -41,45 +41,18 @@ func ListenAndServe(ctx context.Context, addr string, apiToken string, vodEngine
 	return server.Shutdown(ctx)
 }
 
-func NewCatalystAPIRouter(vodEngine *pipeline.Coordinator, apiToken string) *httprouter.Router {
+func NewCatalystAPIRouter(cli config.Cli, bal balancer.Balancer, c cluster.Cluster) *httprouter.Router {
 	router := httprouter.New()
 	withLogging := middleware.LogRequest()
-	withAuth := middleware.IsAuthorized
-	withCapacityChecking := middleware.HasCapacity
 
-	catalystApiHandlers := &handlers.CatalystAPIHandlersCollection{VODEngine: vodEngine}
-	mistCallbackHandlers := &misttriggers.MistCallbackHandlersCollection{VODEngine: vodEngine}
+	geoHandlers := &geolocation.GeolocationHandlersCollection{
+		Balancer: bal,
+		Cluster:  c,
+		Config:   cli,
+	}
 
-	// Simple endpoint for healthchecks
-	router.GET("/ok", withLogging(catalystApiHandlers.Ok()))
-
-	// Public Catalyst API
-	router.POST("/api/vod",
-		withLogging(
-			withAuth(
-				apiToken,
-				withCapacityChecking(
-					vodEngine,
-					catalystApiHandlers.UploadVOD(),
-				),
-			),
-		),
-	)
-
-	router.POST("/api/transcode/file",
-		withLogging(
-			withAuth(
-				apiToken,
-				withCapacityChecking(
-					vodEngine,
-					catalystApiHandlers.TranscodeSegment(),
-				),
-			),
-		),
-	)
-
-	// Endpoint to receive "Triggers" (callbacks) from Mist
-	router.POST("/api/mist/trigger", withLogging(mistCallbackHandlers.Trigger()))
+	// Handling incoming playback redirection requests
+	router.GET("/ok", withLogging(geoHandlers.RedirectHandler()))
 
 	return router
 }
