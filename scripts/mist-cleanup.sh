4810023e1be209a1c0d6d5991a2639aec055fe32
emranemran <emran.mah@gmail.com>
2023-03-23 13:41:41 -0700

    mist-cleanup: fix ShellCheck errors

--- scripts/mist-cleanup.sh
+++ scripts/mist-cleanup.sh
@@ -7,49 +7,49 @@ containsElement () {
   return 1
 }
 
-SEMS=`ls -tr /dev/shm/sem.MstTRKS* /dev/shm/sem.MstUser* /dev/shm/sem.MstInpt* /dev/shm/sem.MstPull* 2> /dev/null`
-PULS=`ls -tr /dev/shm/sem.MstPull* 2> /dev/null`
+SEMS=$(ls -tr /dev/shm/sem.MstTRKS* /dev/shm/sem.MstUser* /dev/shm/sem.MstInpt* /dev/shm/sem.MstPull* 2> /dev/null)
+PULS=$(ls -tr /dev/shm/sem.MstPull* 2> /dev/null)
 #sem.MstPull_golive+Preview
-PAGS=`find /dev/shm/ -name MstData* -o -name MstMeta* -o -name MstTrak* 2> /dev/null`
-STAT=`ls -tr /dev/shm/MstSTATEgolive* 2> /dev/null`
-ACTS=`ps -C MistInBuffer -o command= | awk ' { print $3 } ' | sort | uniq`
-DTPL=`ps -C MistInDTSC -o command= | awk ' { print $3 } ' | sort | uniq`
+PAGS=$(find /dev/shm/ -name "MstData*" -o -name "MstMeta*" -o -name "MstTrak*" 2> /dev/null)
+STAT=$(ls -tr /dev/shm/MstSTATEgolive* 2> /dev/null)
+ACTS=$(ps -C MistInBuffer -o command= | awk ' { print $3 } ' | sort | uniq)
+DTPL=$(ps -C MistInDTSC -o command= | awk ' { print $3 } ' | sort | uniq)
 DATA_DEL=0
 
 #Check for input semaphores without data
 for S in $SEMS ; do
   STRM=${S:20}
-  if containsElement $STRM $ACTS ; then
+  if containsElement "$STRM" "$ACTS" ; then
     echo "Yes: ${STRM} (${S})" > /dev/null
   else
     echo "SEM_INPUT: ${S}"
-    rm ${S}
+    rm "${S}"
   fi
 done
 
 #Check for pull semaphores without data
 for S in $PULS ; do
   STRM=${S:21}
-  if containsElement $STRM $DTPL ; then
+  if containsElement "$STRM" "$DTPL" ; then
     echo "Yes: ${STRM} (${S})" > /dev/null
   else
     #Silenced, since this one is mostly harmless.
     #echo "SEM_PULL: ${S}"
-    rm ${S}
+    rm "${S}"
   fi
 done
 
 #Check for data pages without buffer
-for P in $PAGS ; do
+for P in $PAGS; do
   FILE=${P%@*} #Remove everything after the '@'
   STRM=${FILE:16} #Strip the beginning
-  if containsElement $STRM $ACTS ; then
+  if containsElement "$STRM" "$ACTS" ; then
     echo "Yes: ${STRM} (${P})" > /dev/null
   else
     if (( $# != 0 )); then
       echo "DATA: ${P}"
     fi
-    rm ${P}
+    rm "${P}"
     DATA_DEL=$(( DATA_DEL + 1 ))
   fi
 done
@@ -57,11 +57,11 @@ done
 #Check for state pages without buffer
 for P in $STAT ; do
   STRM=${P:17} #Strip the beginning
-  if containsElement $STRM $ACTS ; then
+  if containsElement "$STRM" "$ACTS" ; then
     echo "Yes: ${STRM} (${P})" > /dev/null
   else
     echo "STATE: ${P}"
-    rm ${P}
+    rm "${P}"
   fi
 done
 
