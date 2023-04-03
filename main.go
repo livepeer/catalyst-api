8819ed23e85e0d0815e356a2ebeffb368a8401d2
emranemran <emran.mah@gmail.com>
2023-04-02 22:33:39 -0700

    mist-cleanup: disable script since it hogged resources and affected streaming

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 	"github.com/livepeer/catalyst-api/cluster"
 	"github.com/livepeer/catalyst-api/config"
 	mistapiconnector "github.com/livepeer/catalyst-api/mapic"
-	"github.com/livepeer/catalyst-api/middleware"
+	//"github.com/livepeer/catalyst-api/middleware"
 	"github.com/livepeer/catalyst-api/pipeline"
 	"github.com/livepeer/livepeer-data/pkg/mistconnector"
 	"github.com/peterbourgon/ff/v3"
@@ -153,15 +153,18 @@ func main() {
 		glog.Fatalf("Error creating VOD pipeline coordinator: %v", err)
 	}
 
-	// Start cron style apps to run periodically
-	app := "mist-cleanup.sh"
-	// schedule mist-cleanup every 2hrs with a timeout of 15min
-	mistCleanup, err := middleware.NewShell(2*60*60*time.Second, 15*60*time.Second, app)
-	if err != nil {
-		glog.Info("Failed to shell out:", app, err)
-	}
-	mistCleanupTick := mistCleanup.RunBg()
-	defer mistCleanupTick.Stop()
+	// disabling cleanup script until we root-cause streaming failures
+	/*
+		// Start cron style apps to run periodically
+		app := "mist-cleanup.sh"
+		// schedule mist-cleanup every 2hrs with a timeout of 15min
+		mistCleanup, err := middleware.NewShell(2*60*60*time.Second, 15*60*time.Second, app)
+		if err != nil {
+			glog.Info("Failed to shell out:", app, err)
+		}
+		mistCleanupTick := mistCleanup.RunBg()
+		defer mistCleanupTick.Stop()
+	*/
 
 	var mapic mistapiconnector.IMac
 	if cli.ShouldMapic() {
