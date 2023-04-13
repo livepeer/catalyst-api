550024402eae42f129018b534ae9755a4863f762
Eli Mallon <eli@livepeer.org>
2023-04-13 18:11:54 +0100

    schema: working event signing!

--- schema/0.0.1/schema_test.go
+++ schema/0.0.1/schema_test.go
@@ -1,42 +1,17 @@
 package schema
 
 import (
+	"encoding/json"
 	"math/big"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/signer/core/apitypes"
 	"github.com/livepeer/catalyst-api/events"
+	"github.com/stretchr/testify/require"
 )
 
-// const primaryType = "Mail"
-//
-// var domainStandard = apitypes.TypedDataDomain{
-//  Name:    "Livepeer Decentralized Video Protocol",
-//  Version: "1",
-//  Salt:    "f8b3858ac49ca50b138587d5dace09bd102b9d24d2567d9a5cde2f6122810931",
-// }
-//
-// var messageStandard = map[string]interface{}{
-//  "from": map[string]interface{}{
-//  "name":   "Cow",
-//  "wallet": "0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826",
-//  },
-//  "to": map[string]interface{}{
-//  "name":   "Bob",
-//  "wallet": "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB",
-//  },
-//  "contents": "Hello, Bob!",
-// }
-//
-// var typedData = apitypes.TypedData{
-//  Types:       typesStandard,
-//  PrimaryType: primaryType,
-//  Domain:      domainStandard,
-//  Message:     messageStandard,
-// }
-
-func TestEventParseVerify(t *testing.T) {
-	var messageStandard = map[string]interface{}{
+func TestSign(t *testing.T) {
+	signer := events.Signer{Types: Types}
+	var testMessage = map[string]interface{}{
 		"time": big.NewInt(1681403259137),
 		"data": map[string]any{
 			"id": "my-awesome-stream",
@@ -47,12 +22,13 @@ func TestEventParseVerify(t *testing.T) {
 			},
 		},
 	}
-	var typedData = apitypes.TypedData{
-		Types:       Types,
+	event := events.Event{
 		PrimaryType: "EventChannelDefinitionMeta",
 		Domain:      Domain,
-		Message:     messageStandard,
+		Message:     testMessage,
 	}
-	out := events.Sign(typedData)
-	panic(out)
+	signedEvent := signer.Sign(event)
+	b, err := json.Marshal(signedEvent)
+	require.NoError(t, err)
+	panic(string(b))
 }
