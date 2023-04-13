b7269994104da0385843c5829decd835049f127c
Eli Mallon <eli@livepeer.org>
2023-04-13 18:11:54 +0100

    schema: working event signing!

--- events/events.go
+++ events/events.go
@@ -1,7 +1,6 @@
 package events
 
 import (
-	"encoding/json"
 	"fmt"
 	"math/big"
 
@@ -11,12 +10,37 @@ import (
 	"github.com/livepeer/go-livepeer/eth"
 )
 
-type fullMessage struct {
-	Data      apitypes.TypedData `json:"data"`
-	Signature string             `json:"sig"`
+// using apitypes.TypedDataDomain directly causes us to get "chainId": null
+type Domain struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+	Salt    string `json:"salt"`
 }
 
-func Sign(typedData apitypes.TypedData) string {
+func (t *Domain) TypedDataDomain() apitypes.TypedDataDomain {
+	return apitypes.TypedDataDomain{
+		Name:    t.Name,
+		Version: t.Version,
+		Salt:    t.Salt,
+	}
+}
+
+type Event struct {
+	PrimaryType string         `json:"primaryType"`
+	Domain      Domain         `json:"domain"`
+	Message     map[string]any `json:"message"`
+}
+
+type SignedEvent struct {
+	Signature string `json:"sig"`
+	Event     Event  `json:"event"`
+}
+
+type Signer struct {
+	Types apitypes.Types
+}
+
+func (s *Signer) Sign(event Event) SignedEvent {
 	id := new(big.Int).SetInt64(int64(80001))
 	am, err := eth.NewAccountManager(ethcommon.HexToAddress(""), ".", id, "secretpassword")
 	if err != nil {
@@ -26,17 +50,20 @@ func Sign(typedData apitypes.TypedData) string {
 	if err != nil {
 		panic(err)
 	}
+	typedData := apitypes.TypedData{
+		Types:       s.Types,
+		PrimaryType: event.PrimaryType,
+		Domain:      event.Domain.TypedDataDomain(),
+		Message:     event.Message,
+	}
 	b, err := am.SignTypedData(typedData)
 	if err != nil {
 		panic(fmt.Errorf("error signing typed data: %w", err))
 	}
-	message := fullMessage{
-		Data:      typedData,
-		Signature: fmt.Sprintf("%s", hexutil.Bytes(b)),
+	// golint wants string(b) but that gives /x1234 encoded output
+	sig := fmt.Sprintf("%s", hexutil.Bytes(b)) //nolint:gosimple
+	return SignedEvent{
+		Event:     event,
+		Signature: sig,
 	}
-	b2, err := json.Marshal(message)
-	if err != nil {
-		panic(err)
-	}
-	return string(b2)
 }
--- schema/0.0.1/schema.go
+++ schema/0.0.1/schema.go
@@ -1,9 +1,12 @@
 package schema
 
-import "github.com/ethereum/go-ethereum/signer/core/apitypes"
+import (
+	"github.com/ethereum/go-ethereum/signer/core/apitypes"
+	"github.com/livepeer/catalyst-api/events"
+)
 
 // TODO: Not sure what we need here yet exactly... salt good? Definitely don't want the chainID for chain-agnostic reasons
-var Domain = apitypes.TypedDataDomain{
+var Domain = events.Domain{
 	Name:    "Livepeer Decentralized Video Protocol",
 	Version: "0.0.1",
 	Salt:    "f8b3858ac49ca50b138587d5dace09bd102b9d24d2567d9a5cde2f6122810931",
