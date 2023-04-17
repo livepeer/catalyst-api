dbb802d196a80dd5dc9f1bab5acdbdc82e433d63
Thom Shutt <thomshutt@users.noreply.github.com>
2023-04-17 17:05:58 +0100

    Fix method signature

--- video/segment.go
+++ video/segment.go
@@ -6,7 +6,7 @@ import (
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
-func Segment(requestID string, sourceURL string, outputManifestURL string, targetSegmentSize int64) error {
+func Segment(sourceURL string, outputManifestURL string, targetSegmentSize int64) error {
 	err := ffmpeg.Input(sourceURL).
 		Output(
 			outputManifestURL,
