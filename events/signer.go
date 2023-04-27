d9e57448dbb0dc7008434555535d17b00026d158
Eli Mallon <eli@livepeer.org>
2023-04-27 17:34:46 +0200

    events: working refactored sign/verify flow

--- events/signer.go
+++ events/signer.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"math/big"
 
-	"github.com/ethereum/go-ethereum/common"
 	ethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -14,48 +13,97 @@ import (
 
 // schema-aware signer for signing actions and verifying untrusted payloads
 type Signer struct {
-	Types apitypes.Types
+	// When I sign an action, which schema should I use?
+	PrimarySchema *Schema
+	// All supported schemas for verification purposes
+	Schemas []*Schema
 }
 
-func (s *Signer) Sign(unverified UnverifiedEvent) SignedEvent {
+func (s *Signer) Sign(action Action) (*SignedEvent, error) {
 	id := new(big.Int).SetInt64(int64(80001))
 	am, err := eth.NewAccountManager(ethcommon.HexToAddress(""), ".", id, "secretpassword")
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	err = am.Unlock("secretpassword")
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	typedData := unverified.TypedData(s.Types)
+	typedData := apitypes.TypedData{
+		Types:       s.PrimarySchema.Types,
+		Domain:      s.PrimarySchema.Domain.TypedDataDomain(),
+		PrimaryType: action.Type(),
+		Message:     ActionToMap(action),
+	}
+	_, err = typedData.HashStruct("EIP712Domain", typedData.Domain.Map())
+	if err != nil {
+		return nil, fmt.Errorf("error signing EIP712Domain: %w", err)
+	}
+	_, err = typedData.HashStruct(typedData.PrimaryType, typedData.Message)
+	if err != nil {
+		return nil, fmt.Errorf("error signing struct: %w", err)
+	}
+
 	b, err := am.SignTypedData(typedData)
 	if err != nil {
-		panic(fmt.Errorf("error signing typed data: %w", err))
+		return nil, fmt.Errorf("error signing typed data: %w", err)
 	}
 	// golint wants string(b) but that gives /x1234 encoded output
 	sig := fmt.Sprintf("%s", hexutil.Bytes(b)) //nolint:gosimple
-	return SignedEvent{
-		PrimaryType: unverified.PrimaryType,
-		Domain:      unverified.Domain,
-		Signature:   sig,
-		Address:     am.Account().Address,
-	}
+	return &SignedEvent{
+		Domain:    s.PrimarySchema.Domain,
+		Signature: sig,
+		Address:   am.Account().Address,
+		Action:    action,
+	}, nil
 }
 
-func (s *Signer) Verify(unverified UnverifiedEvent) (SignedEvent, error) {
+func (s *Signer) Verify(unverified UnverifiedEvent) (*SignedEvent, error) {
+	// find the correct schema for this action
+	var schema *Schema
+	for _, s := range s.Schemas {
+		eq, err := s.Domain.Equal(&unverified.Domain)
+		if eq && err == nil {
+			schema = s
+			break
+		}
+	}
+	if schema == nil {
+		return nil, fmt.Errorf("unknown event domain: %s", unverified.Domain)
+	}
 	sig, err := hexutil.Decode(unverified.Signature)
 	sig[64] -= 27
 	if err != nil {
-		return SignedEvent{}, err
+		return nil, err
 	}
-	typedData := signedEvent.Event.TypedData(s.Types)
+	typedData := apitypes.TypedData{
+		Types:       schema.Types,
+		Domain:      schema.Domain.TypedDataDomain(),
+		PrimaryType: unverified.PrimaryType,
+		Message:     unverified.Message,
+	}
 	hash, _, err := apitypes.TypedDataAndHash(typedData)
 	if err != nil {
-		return common.Address{}, err
+		return nil, err
 	}
 	rpk, err := crypto.SigToPub(hash, sig)
 	if err != nil {
-		return common.Address{}, err
+		return nil, err
 	}
-	return crypto.PubkeyToAddress(*rpk), nil
+	addr := crypto.PubkeyToAddress(*rpk)
+	actionGenerator, ok := schema.Actions[unverified.PrimaryType]
+	if !ok {
+		return nil, fmt.Errorf("unknown action domain: %s", unverified.Domain)
+	}
+	action := actionGenerator()
+	err = LoadMap(action, unverified.Message)
+	if err != nil {
+		return nil, err
+	}
+	return &SignedEvent{
+		Domain:    schema.Domain,
+		Signature: unverified.Signature,
+		Address:   addr,
+		Action:    action,
+	}, nil
 }
