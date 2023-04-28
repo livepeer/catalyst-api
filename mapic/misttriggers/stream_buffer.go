fc403d51e338e135d5a5fadf4aad00e869ba35cf
Victor Elias <victorgelias@gmail.com>
2023-04-28 15:11:12 -0300

    mapic: Add STREAM_BUFFER handling for stream health reporting (#638)
    * mapic: Create logging STREAM_BUFFER trigger handler
    
    This was supposed to be in catalyst-api, but it doens't
    connect to Mist anymore, so it's just easier to add it
    to mapic.
    
    * mapic: Plug the stream buffer handler
    
    * mapic: Make misttrigger catalyst-api-free
    
    Even tho we want to migrate there, we shouldn't
    have dependencies between these 2
    
    * mapic: Push stream buffer info to a health hook
    
    * mapic: Fix lint
    
    * mapic: Add omitempty to the optional stream health fields
    
    * mapic/misttriggers: Add a bunch of tests
    
    I just love gpt
    
    * stream_buffer: Replace mist state w/ IsActive&Healthy
    
    * stream_buffer: Make stream health payload snake_case
    
    That's the catalyst-api standard so let's stick to that here
    
    * mapic: Fix tests
    
    * stream_buffer: Clean-up test inputs
    
    * fix linter error
    
    ---------
    
    Co-authored-by: Max Holland <max@livepeer.org>

---
+++ mapic/misttriggers/stream_buffer.go
@@ -0,0 +1,192 @@
+package misttriggers
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/golang/glog"
+	"github.com/hashicorp/go-retryablehttp"
+	"github.com/livepeer/catalyst-api/config"
+)
+
+var hookClient *http.Client
+
+func init() {
+	client := retryablehttp.NewClient()
+	client.RetryMax = 2                   // Attempte request a maximum of this+1 times
+	client.RetryWaitMin = 1 * time.Second // Wait at least this long between retries
+	client.RetryWaitMax = 5 * time.Second // Wait at most this long between retries (exponential backoff)
+	client.HTTPClient = &http.Client{
+		Timeout: 5 * time.Second, // Give up on requests that take more than this long
+	}
+
+	hookClient = client.StandardClient()
+}
+
+// This was originally written as catalalyst-api code, since that's where it's
+// supposed to go. Currently, catalyst-api has no connection with Mist anymore
+// though, so it's easier to plug this into mist-api-connector instead for now.
+// TODO: Move this to catalyst-api when its got the Mist plumbing back.
+
+// This trigger is run whenever the live buffer state of a stream changes. It is
+// not ran for VoD streams. This trigger is stream-specific and non-blocking.
+//
+// The payload for this trigger is multiple lines, each separated by a single
+// newline character (without an ending newline), containing data as such:
+//
+// stream name
+// stream state (one of: FULL, EMPTY, DRY, RECOVER)
+// {JSON object with stream details, only when state is not EMPTY}
+//
+// Read the Mist documentation for more details on each of the stream states.
+func TriggerStreamBuffer(cli *config.Cli, req *http.Request, lines []string) error {
+	sessionID := req.Header.Get("X-UUID")
+
+	body, err := ParseStreamBufferPayload(lines)
+	if err != nil {
+		glog.Infof("Error parsing STREAM_BUFFER payload error=%q payload=%s", err, strings.Join(lines, "\n"))
+		return err
+	}
+
+	rawBody, _ := json.Marshal(body)
+	glog.Infof("Got STREAM_BUFFER trigger sessionId=%q payload=%s", sessionID, rawBody)
+
+	streamHealth := StreamHealthPayload{
+		StreamName: body.StreamName,
+		SessionID:  sessionID,
+		IsActive:   body.State != "EMPTY",
+		IsHealthy:  body.State == "FULL" || body.State == "RECOVER",
+	}
+	if details := body.Details; details != nil {
+		streamHealth.IsHealthy = streamHealth.IsHealthy && details.Issues == ""
+		streamHealth.Tracks = details.Tracks
+		streamHealth.Issues = details.Issues
+	}
+
+	err = PostStreamHealthPayload(cli.StreamHealthHookURL, cli.APIToken, streamHealth)
+	if err != nil {
+		glog.Infof("Error pushing STREAM_HEALTH payload error=%q payload=%s", err, rawBody)
+		return err
+	}
+
+	return nil
+}
+
+type StreamHealthPayload struct {
+	StreamName string                  `json:"stream_name"`
+	SessionID  string                  `json:"session_id"`
+	IsActive   bool                    `json:"is_active"`
+	IsHealthy  bool                    `json:"is_healthy"`
+	Tracks     map[string]TrackDetails `json:"tracks,omitempty"`
+	Issues     string                  `json:"issues,omitempty"`
+}
+
+func PostStreamHealthPayload(url, apiToken string, payload StreamHealthPayload) error {
+	body, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("error marshalling stream health payload: %w", err)
+	}
+	req, err := http.NewRequest("POST", url, bytes.NewReader(body))
+	if err != nil {
+		return fmt.Errorf("error creating stream health request: %w", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+apiToken)
+
+	resp, err := hookClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("error pushing stream health to hook: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 400 {
+		respBody, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return fmt.Errorf("error reading stream health hook response: %w", err)
+		}
+		glog.Warningf("Error pushing stream health to hook status=%d body=%q", resp.StatusCode, respBody)
+	}
+
+	return nil
+}
+
+type StreamBufferPayload struct {
+	StreamName string
+	State      string
+	Details    *MistStreamDetails
+}
+
+type TrackDetails struct {
+	Codec  string                 `json:"codec"`
+	Kbits  int                    `json:"kbits"`
+	Keys   map[string]interface{} `json:"keys"`
+	Fpks   int                    `json:"fpks,omitempty"`
+	Height int                    `json:"height,omitempty"`
+	Width  int                    `json:"width,omitempty"`
+}
+
+func ParseStreamBufferPayload(lines []string) (*StreamBufferPayload, error) {
+	if len(lines) < 2 || len(lines) > 3 {
+		return nil, fmt.Errorf("invalid payload: expected 2 or 3 lines but got %d", len(lines))
+	}
+
+	streamName := lines[0]
+	streamState := lines[1]
+	var streamDetailsStr string
+	if len(lines) == 3 {
+		streamDetailsStr = lines[2]
+	}
+
+	streamDetails, err := ParseMistStreamDetails(streamState, []byte(streamDetailsStr))
+	if err != nil {
+		return nil, fmt.Errorf("error parsing stream details JSON: %w", err)
+	}
+
+	return &StreamBufferPayload{
+		StreamName: streamName,
+		State:      streamState,
+		Details:    streamDetails,
+	}, nil
+}
+
+type MistStreamDetails struct {
+	Tracks map[string]TrackDetails
+	Issues string
+}
+
+// Mists saves the tracks and issues in the same JSON object, so we need to
+// parse them separately. e.g. {track-id-1: {...}, issues: "..."}
+func ParseMistStreamDetails(streamState string, data []byte) (*MistStreamDetails, error) {
+	if streamState == "EMPTY" {
+		return nil, nil
+	}
+
+	var tracksAndIssues map[string]interface{}
+	err := json.Unmarshal(data, &tracksAndIssues)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing stream details JSON: %w", err)
+	}
+
+	issues, ok := tracksAndIssues["issues"].(string)
+	if raw, keyExists := tracksAndIssues["issues"]; keyExists && !ok {
+		return nil, fmt.Errorf("issues field is not a string: %v", raw)
+	}
+	delete(tracksAndIssues, "issues")
+
+	tracksJSON, err := json.Marshal(tracksAndIssues) // only tracks now
+	if err != nil {
+		return nil, fmt.Errorf("error marshalling stream details tracks: %w", err)
+	}
+
+	var tracks map[string]TrackDetails
+	if err = json.Unmarshal(tracksJSON, &tracks); err != nil {
+		return nil, fmt.Errorf("eror parsing stream details tracks: %w", err)
+	}
+
+	return &MistStreamDetails{tracks, issues}, nil
+}
---
+++ mapic/misttriggers/stream_buffer_test.go
@@ -0,0 +1,144 @@
+package misttriggers
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/livepeer/catalyst-api/config"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	streamBufferPayloadFull = []string{
+		"stream1", "FULL", `{"track1":{"codec":"h264","kbits":1000,"keys":{"B":"1"},"fpks":30,"height":720,"width":1280}}`,
+	}
+	streamBufferPayloadIssues = []string{
+		"stream1", "RECOVER", `{"track1":{"codec":"h264","kbits":1000,"keys":{"B":"1"},"fpks":30,"height":720,"width":1280},"issues":"Stream is feeling under the weather"}`,
+	}
+	streamBufferPayloadInvalid = []string{
+		"stream1", "FULL", `{"track1":{},"issues":false}`,
+	}
+	streamBufferPayloadEmpty = []string{"stream1", "EMPTY"}
+)
+
+func TestItCanParseAValidStreamBufferPayload(t *testing.T) {
+	p, err := ParseStreamBufferPayload(streamBufferPayloadFull)
+	require.NoError(t, err)
+	require.Equal(t, p.StreamName, "stream1")
+	require.Equal(t, p.State, "FULL")
+	require.NotNil(t, p.Details)
+	require.Equal(t, p.Details.Issues, "")
+	require.Len(t, p.Details.Tracks, 1)
+	require.Contains(t, p.Details.Tracks, "track1")
+}
+
+func TestItCanParseAStreamBufferPayloadWithStreamIssues(t *testing.T) {
+	p, err := ParseStreamBufferPayload(streamBufferPayloadIssues)
+	require.NoError(t, err)
+	require.Equal(t, p.StreamName, "stream1")
+	require.Equal(t, p.State, "RECOVER")
+	require.NotNil(t, p.Details)
+	require.Equal(t, p.Details.Issues, "Stream is feeling under the weather")
+	require.Len(t, p.Details.Tracks, 1)
+	require.Contains(t, p.Details.Tracks, "track1")
+}
+
+func TestItCanParseAValidStreamBufferPayloadWithEmptyState(t *testing.T) {
+	p, err := ParseStreamBufferPayload(streamBufferPayloadEmpty)
+	require.NoError(t, err)
+	require.Equal(t, p.StreamName, "stream1")
+	require.Equal(t, p.State, "EMPTY")
+	require.Nil(t, p.Details)
+}
+
+func TestItFailsToParseAnInvalidStreamBufferPayload(t *testing.T) {
+	_, err := ParseStreamBufferPayload(streamBufferPayloadInvalid)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "issues field is not a string")
+}
+
+func TestPostStreamHealthPayloadFailsWithInvalidURL(t *testing.T) {
+	streamHealthPayload := StreamHealthPayload{
+		StreamName: "stream1",
+		SessionID:  "session1",
+		IsActive:   true,
+		IsHealthy:  true,
+		Tracks:     nil,
+		Issues:     "",
+	}
+
+	err := PostStreamHealthPayload("http://invalid.url", "apiToken", streamHealthPayload)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "error pushing stream health to hook")
+}
+
+func TestPostStreamHealthPayloadWithTestServer(t *testing.T) {
+	// Start an HTTP test server to simulate the webhook endpoint
+	callCount := 0
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		callCount++
+		w.WriteHeader(http.StatusNoContent)
+	}))
+	defer server.Close()
+
+	streamHealthPayload := StreamHealthPayload{
+		StreamName: "stream1",
+		SessionID:  "session1",
+		IsActive:   true,
+		IsHealthy:  true,
+		Tracks:     nil,
+		Issues:     "No issues",
+	}
+
+	err := PostStreamHealthPayload(server.URL, "apiToken", streamHealthPayload)
+	require.NoError(t, err)
+	require.Equal(t, 1, callCount)
+}
+
+func TestTriggerStreamBufferE2E(t *testing.T) {
+	// Start an HTTP test server to simulate the webhook endpoint
+	var receivedPayload StreamHealthPayload
+	var receivedAuthHeader string
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		receivedAuthHeader = r.Header.Get("Authorization")
+
+		defer r.Body.Close()
+		err := json.NewDecoder(r.Body).Decode(&receivedPayload)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			_, err := w.Write([]byte("error unmarshalling payload"))
+			require.NoError(t, err)
+			return
+		}
+
+		w.WriteHeader(http.StatusNoContent)
+	}))
+	defer server.Close()
+
+	// Prepare the request and payload
+	payload := strings.NewReader(strings.Join(streamBufferPayloadIssues, "\n"))
+	req, err := http.NewRequest("GET", "http://example.com", payload)
+	require.NoError(t, err)
+	req.Header.Set("X-UUID", "session1")
+
+	// Call the TriggerStreamBuffer function
+	cli := &config.Cli{
+		StreamHealthHookURL: server.URL,
+		APIToken:            "apiToken",
+	}
+	err = TriggerStreamBuffer(cli, req, streamBufferPayloadIssues)
+	require.NoError(t, err)
+
+	// Check the payload received by the test server
+	require.Equal(t, receivedAuthHeader, "Bearer apiToken")
+	require.Equal(t, receivedPayload.StreamName, "stream1")
+	require.Equal(t, receivedPayload.SessionID, "session1")
+	require.Equal(t, receivedPayload.IsActive, true)
+	require.Equal(t, receivedPayload.IsHealthy, false)
+	require.Len(t, receivedPayload.Tracks, 1)
+	require.Contains(t, receivedPayload.Tracks, "track1")
+	require.Equal(t, receivedPayload.Issues, "Stream is feeling under the weather")
+}
