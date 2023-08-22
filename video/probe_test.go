53cd1e61e375534073e1ba7e9d0fe61b79a27fd6
Max Holland <max@livepeer.org>
2023-08-22 10:41:35 +0100

    add test

--- video/probe_test.go
+++ video/probe_test.go
@@ -132,3 +132,34 @@ func TestProbe_IgnoreSomeErrors(t *testing.T) {
 	_, err = Probe{IgnoreErrMessages: []string{"non-existing pps 0 referenced"}}.ProbeFile("requestID", "./fixtures/non-existing-pps.ts")
 	require.NoError(t, err)
 }
+
+func Test_findFormat(t *testing.T) {
+	tests := []struct {
+		name   string
+		format string
+		want   string
+	}{
+		{
+			name:   "single format",
+			format: "mp4",
+			want:   "mp4",
+		},
+		{
+			name:   "empty format",
+			format: "",
+			want:   "",
+		},
+		{
+			name:   "multiple format",
+			format: "mov,hello",
+			want:   "mov",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findFormat(tt.format); got != tt.want {
+				t.Errorf("findFormat() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
