be31fe1a3cbbd572f6b4357cd3389e78cac53d04
Thom Shutt <thomshutt@users.noreply.github.com>
2023-08-29 16:03:36 +0100

    Bring in-flight HTTP requests into capacity calculation

--- middleware/capacity.go
+++ middleware/capacity.go
@@ -2,18 +2,27 @@ package middleware
 
 import (
 	"net/http"
+	"sync/atomic"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/livepeer/catalyst-api/config"
 	"github.com/livepeer/catalyst-api/pipeline"
 )
 
-func HasCapacity(vodEngine *pipeline.Coordinator, next httprouter.Handle) httprouter.Handle {
+type CapacityMiddleware struct {
+	requestsInFlight atomic.Int64
+}
+
+func (c *CapacityMiddleware) HasCapacity(vodEngine *pipeline.Coordinator, next httprouter.Handle) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		if len(vodEngine.Jobs.GetKeys()) >= config.MAX_JOBS_IN_FLIGHT {
+		requestsInFlight := c.requestsInFlight.Add(1)
+		defer c.requestsInFlight.Add(-1)
+
+		if len(vodEngine.Jobs.GetKeys())+int(requestsInFlight) >= config.MAX_JOBS_IN_FLIGHT {
 			w.WriteHeader(http.StatusTooManyRequests)
 			return
 		}
+
 		next(w, r, ps)
 	}
 }
