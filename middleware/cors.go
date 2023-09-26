70dfb6fd1caebaa173fb6811357a4f0e351ae70c
Eli Mallon <eli@livepeer.org>
2023-09-26 11:11:34 -0700

    cors: add Access-Control-Allow-Methods: everything

--- middleware/cors.go
+++ middleware/cors.go
@@ -11,6 +11,8 @@ func AllowCORS() func(httprouter.Handle) httprouter.Handle {
 		handler := func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.Header().Set("Access-Control-Allow-Headers", "*")
+			// Safari doesn't allow a wildcard for this so we just list them all
+			w.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE")
 
 			next(w, r, ps)
 		}
