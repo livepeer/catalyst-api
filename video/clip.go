2368855e409a1de480043781325b5d479344874e
emranemran <emran.mah@gmail.com>
2023-09-27 17:01:27 -0700

    clip-segment

--- video/clip.go
+++ video/clip.go
@@ -15,6 +15,12 @@ type ClipStrategy struct {
 	PlaybackID string `json:"playback_id,omitempty"` // playback-id of asset to clip
 }
 
+type ClipSegmentInfo struct {
+	SequenceID     uint64
+	Duration       float64
+	ClipOffsetSecs float64
+}
+
 // format time in secs to be copatible with ffmpeg's expected time syntax
 func formatTime(timeSeconds float64) string {
 	timeMillis := int64(timeSeconds * 1000)
@@ -37,7 +43,7 @@ func GetTotalDurationAndSegments(manifest *m3u8.MediaPlaylist) (float64, uint64)
 }
 
 // Finds the segment in an HLS manifest that contains the timestamp (aka playhead) specified.
-func getRelevantSegment(allSegments []*m3u8.MediaSegment, playHeadTime float64, startingIdx uint64) (uint64, error) {
+func getRelevantSegment(allSegments []*m3u8.MediaSegment, playHeadTime float64, startingIdx uint64) (ClipSegmentInfo, error) {
 	playHeadDiff := 0.0
 
 	for _, segment := range allSegments[startingIdx:] {
@@ -47,7 +53,7 @@ func getRelevantSegment(allSegments []*m3u8.MediaSegment, playHeadTime float64, 
 		}
 		// Skip any segments where duration is 0s
 		if segment.Duration <= 0.0 {
-			return 0, fmt.Errorf("error clipping: found 0s duration segments")
+			return ClipSegmentInfo{}, fmt.Errorf("error clipping: found 0s duration segments")
 		}
 		// Check if the playhead is within the current segment and skip to
 		// the next segment if it's not. Also update the play head by referencing
@@ -58,9 +64,11 @@ func getRelevantSegment(allSegments []*m3u8.MediaSegment, playHeadTime float64, 
 			continue
 		}
 		// If we reach here, then we've found the relevant segment that falls within the playHeadTime
-		return segment.SeqId, nil
+		return ClipSegmentInfo{SequenceID: segment.SeqId,
+			Duration:       segment.Duration,
+			ClipOffsetSecs: playHeadTime}, nil
 	}
-	return 0, fmt.Errorf("error clipping: did not find a segment that falls within %v seconds", playHeadTime)
+	return ClipSegmentInfo{}, fmt.Errorf("error clipping: did not find a segment that falls within %v seconds", playHeadTime)
 }
 
 func ConvertUnixMillisToSeconds(requestID string, firstSegment *m3u8.MediaSegment, startTimeUnixMillis, endTimeUnixMillis int64) (float64, float64, error) {
@@ -77,7 +85,7 @@ func ConvertUnixMillisToSeconds(requestID string, firstSegment *m3u8.MediaSegmen
 	startTimeSeconds := float64(startTimeUnixMillis-firstSegUnixMillis) / 1000.0
 	endTimeSeconds := float64(endTimeUnixMillis-firstSegUnixMillis) / 1000.0
 
-	log.Log(requestID, "Clipping timestamps",
+	log.Log(requestID, "XXX: Clipping timestamps",
 		"start-PROGRAM-DATE-TIME-UTC", firstSegProgramDateTimeUTC,
 		"UNIX-time-milliseconds", firstSegUnixMillis,
 		"start-time-unix-milliseconds", startTimeUnixMillis,
@@ -89,8 +97,9 @@ func ConvertUnixMillisToSeconds(requestID string, firstSegment *m3u8.MediaSegmen
 }
 
 // Function to find relevant segments that span from the clipping start and end times
-func ClipManifest(requestID string, manifest *m3u8.MediaPlaylist, startTime, endTime float64) ([]*m3u8.MediaSegment, error) {
-	var startSegIdx, endSegIdx uint64
+func ClipManifest(requestID string, manifest *m3u8.MediaPlaylist, startTime, endTime float64) ([]*m3u8.MediaSegment, []ClipSegmentInfo, error) {
+	//var startSegIdx, endSegIdx uint64
+	var clipStartSegmentInfo, clipEndSegmentInfo ClipSegmentInfo
 	var err error
 
 	manifestDuration, manifestSegments := GetTotalDurationAndSegments(manifest)
@@ -98,58 +107,99 @@ func ClipManifest(requestID string, manifest *m3u8.MediaPlaylist, startTime, end
 	// Find the segment index that correlates with the specified startTime
 	// but error out it exceeds the  manifest's duration.
 	if startTime > manifestDuration {
-		return nil, fmt.Errorf("error clipping: start time specified exceeds duration of manifest")
+		return nil, []ClipSegmentInfo{}, fmt.Errorf("error clipping: start time specified exceeds duration of manifest")
 	} else {
-		startSegIdx, err = getRelevantSegment(manifest.Segments, startTime, 0)
+		//startSegIdx, err = getRelevantSegment(manifest.Segments, startTime, 0)
+		clipStartSegmentInfo, err = getRelevantSegment(manifest.Segments, startTime, 0)
 		if err != nil {
-			return nil, fmt.Errorf("error clipping: failed to get a starting index: %w", err)
+			return nil, []ClipSegmentInfo{}, fmt.Errorf("error clipping: failed to get a starting index: %w", err)
 		}
 	}
 
 	// Find the segment index that correlates with the specified endTime.
 	if endTime > manifestDuration {
-		endSegIdx = manifestSegments - 1
+		lastSegmentIdx := manifestSegments - 1
+		lastSegmentDuration := manifest.Segments[lastSegmentIdx].Duration
+		clipEndSegmentInfo = ClipSegmentInfo{SequenceID: lastSegmentIdx, Duration: lastSegmentDuration, ClipOffsetSecs: lastSegmentDuration}
 	} else {
-		endSegIdx, err = getRelevantSegment(manifest.Segments, endTime, startSegIdx)
+		clipEndSegmentInfo, err = getRelevantSegment(manifest.Segments, endTime, clipStartSegmentInfo.SequenceID)
 		if err != nil {
-			return nil, fmt.Errorf("error clipping: failed to get an ending index")
+			return nil, []ClipSegmentInfo{}, fmt.Errorf("error clipping: failed to get an ending index")
 		}
 	}
 
+	firstSegmentToClip := clipStartSegmentInfo.SequenceID
+	lastSegmentToClip := clipEndSegmentInfo.SequenceID
 	// Generate a slice of all segments that overlap with startTime to endTime
-	relevantSegments := manifest.Segments[startSegIdx : endSegIdx+1]
+	relevantSegments := manifest.Segments[firstSegmentToClip : lastSegmentToClip+1]
 	totalRelSegs := len(relevantSegments)
 	if totalRelSegs == 0 {
-		return nil, fmt.Errorf("error clipping: no relevant segments found in the specified time range")
+		return nil, []ClipSegmentInfo{}, fmt.Errorf("error clipping: no relevant segments found in the specified time range")
 	}
 
-	log.Log(requestID, "Clipping segments", "from", startSegIdx, "to", endSegIdx)
+	log.Log(requestID, "XXX: Clipping segments", "from", firstSegmentToClip, "to", lastSegmentToClip)
 
-	return relevantSegments, nil
+	var cs []ClipSegmentInfo
+	if firstSegmentToClip == lastSegmentToClip {
+		cs = []ClipSegmentInfo{clipStartSegmentInfo}
+	} else {
+		cs = []ClipSegmentInfo{clipStartSegmentInfo, clipEndSegmentInfo}
+	}
+	fmt.Println("XXXXXXXX", cs)
+	return relevantSegments, cs, nil
 }
 
-// Clips a segment by re-encoding so that I-frames are placed at the right intervals
-// Currently using these settings for the encode step:
+// Clips a segment from the specified start/end times. In some cases, the segment
+// will be re-encoded so that I-frames are placed at the right intervals.
+// This allows for frame-accurate clipping.
+// Currently using a combination of these settings for the encode step:
 //
 //	"c:v": "libx264": Specifies H.264 video codec.
-//	"r": "24": Sets video frame rate to 24 FPS.
 //	"g": "48": Inserts keyframe every 48 frames (GOP size).
 //	"keyint_min": "48": Minimum keyframe interval.
 //	"sc_threshold": 50: Detects scene changes with threshold 50.
 //	"bf": "0": Disables B-frames for bidirectional prediction.
-//	"c:a": "copy": Copies audio stream without re-encoding.
+//	"c:a": "aac": re-encode audio and clip.
 func ClipSegment(tsInputFile, tsOutputFile string, startTime, endTime float64) error {
-	start := formatTime(startTime)
-	end := formatTime(endTime)
-	err := ffmpeg.Input(tsInputFile).
-		Output(tsOutputFile, ffmpeg.KwArgs{"ss": start, "to": end,
+	var start, end string
+	var args ffmpeg.KwArgs
+	if endTime < 0 {
+		// Clip from specified start time to the end of
+		// the segment (when clipping starting segment)
+		start = formatTime(startTime)
+		args = ffmpeg.KwArgs{"bf": "0",
+			"c:a":          "aac",
 			"c:v":          "libx264",
-			"r":            "24",
 			"g":            "48",
 			"keyint_min":   "48",
 			"sc_threshold": 50,
-			"bf":           "0",
-			"c:a":          "copy"}).
+			"ss":           start}
+	} else if startTime < 0 {
+		// Clip from beginning of segment to specified end time
+		// without re-encoding (when clipping ending segment)
+		start = formatTime(startTime)
+		end = formatTime(endTime)
+		args = ffmpeg.KwArgs{"c:a": "copy",
+			"c:v": "copy",
+			"ss":  "00:00:00.000",
+			"to":  end}
+	} else {
+		// Clip from specified start/end times (when
+		// start/end falls within same segment)
+		start = formatTime(startTime)
+		end = formatTime(endTime)
+		args = ffmpeg.KwArgs{"bf": "0",
+			"c:a":          "aac",
+			"c:v":          "libx264",
+			"g":            "48",
+			"keyint_min":   "48",
+			"sc_threshold": 50,
+			"ss":           start,
+			"to":           end}
+	}
+
+	err := ffmpeg.Input(tsInputFile).
+		Output(tsOutputFile, args).
 		OverWriteOutput().ErrorToStdOut().Run()
 	if err != nil {
 		return fmt.Errorf("failed to clip segments from %s: %w", tsInputFile, err)
