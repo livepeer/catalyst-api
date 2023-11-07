7a5328e4f8bf617213da1f89e0b6d4bda86c4e31
gioelecerati <50955448+gioelecerati@users.noreply.github.com>
2023-11-07 17:42:19 +0100

    cors: allow credentials and remove wildcard to allow cookie access key (#951)
    * cors: allow credentials and remove wildcard to allow cookie access key
    
    * pass wildcard when origin is missing

--- middleware/cors.go
+++ middleware/cors.go
@@ -9,8 +9,13 @@ import (
 func AllowCORS() func(httprouter.Handle) httprouter.Handle {
 	return func(next httprouter.Handle) httprouter.Handle {
 		handler := func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-			w.Header().Set("Access-Control-Allow-Origin", "*")
+			originDomain := r.Header.Get("Origin")
+			if originDomain == "" {
+				originDomain = "*"
+			}
+			w.Header().Set("Access-Control-Allow-Origin", originDomain)
 			w.Header().Set("Access-Control-Allow-Headers", "*")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			// Safari doesn't allow a wildcard for this so we just list them all
 			w.Header().Set("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE")
 
