4e8d895db03c74683c68b572efbcfee6f9b78f40
Max Holland <max@livepeer.org>
2023-11-09 15:08:12 +0000

    Push thumbnail generation to background
    For now keep it simple, at a later date we can make sure we wait for completion at the end of the vod job

--- pipeline/ffmpeg.go
+++ pipeline/ffmpeg.go
@@ -78,10 +78,12 @@ func (f *ffmpeg) HandleStartUploadJob(job *JobInfo) (*HandlerOutput, error) {
 	job.ReportProgress(clients.TranscodeStatusPreparingCompleted, 1)
 
 	if job.ThumbnailsTargetURL != nil {
-		err := thumbnails.GenerateThumbs(job.RequestID, job.SegmentingTargetURL, job.ThumbnailsTargetURL)
-		if err != nil {
-			log.LogError(job.RequestID, "generate thumbs failed", err, "in", job.SegmentingTargetURL, "out", job.ThumbnailsTargetURL)
-		}
+		go func() {
+			err := thumbnails.GenerateThumbs(job.RequestID, job.SegmentingTargetURL, job.ThumbnailsTargetURL)
+			if err != nil {
+				log.LogError(job.RequestID, "generate thumbs failed", err, "in", job.SegmentingTargetURL, "out", job.ThumbnailsTargetURL)
+			}
+		}()
 	}
 
 	// Transcode Beginning
