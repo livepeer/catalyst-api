c7c9a0e381b423343203f3df52d11195f591ac59
Thom Shutt <thomshutt@users.noreply.github.com>
2023-11-15 11:31:52 +0300

    Basic Node / Stream structs

--- balancer/catalyst/catalyst_balancer.go
+++ balancer/catalyst/catalyst_balancer.go
@@ -1,1 +1,12 @@
 package catalyst
+
+// TODO: This is temporary until we have the real struct definition
+type Node struct {
+	Streams                  map[string]Stream // Stream ID -> Stream
+	CPUUsagePercentage       int64
+	RAMUsagePercentage       int64
+	BandwidthUsagePercentage int64
+}
+type Stream struct {
+	ID string
+}
