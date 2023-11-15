07f28b11f93bed638786e8688a82772ab5d2c7ee
Thom Shutt <thomshutt@users.noreply.github.com>
2023-11-15 11:56:08 +0300

    First test and stub implementation of selection algorithm

--- balancer/catalyst/catalyst_balancer.go
+++ balancer/catalyst/catalyst_balancer.go
@@ -1,5 +1,7 @@
 package catalyst
 
+import "fmt"
+
 // TODO: This is temporary until we have the real struct definition
 type Node struct {
 	Streams                  map[string]Stream // Stream ID -> Stream
@@ -9,4 +11,11 @@ type Node struct {
 }
 type Stream struct {
 	ID string
+}
+
+func (n Node) SelectNode(nodes []Node) (Node, error) {
+	if len(nodes) == 0 {
+		return n, nil
+	}
+	return Node{}, fmt.Errorf("not yet implemented")
 }
--- balancer/catalyst/catalyst_balancer_test.go
+++ balancer/catalyst/catalyst_balancer_test.go
@@ -1,1 +1,22 @@
 package catalyst
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestItReturnsItselfWhenNoOtherNodesPresent(t *testing.T) {
+	// Make the node handling the request unfavourable in terms of stats, to make sure
+	// it'll still pick itself if it's the only option
+	currentNode := Node{
+		CPUUsagePercentage:       100,
+		RAMUsagePercentage:       100,
+		BandwidthUsagePercentage: 100,
+		Streams:                  map[string]Stream{},
+	}
+	n, err := currentNode.SelectNode([]Node{})
+	require.NoError(t, err)
+	require.Equal(t, currentNode, n)
+
+}
