ec22be6fced2f4ca339824a26f5076d0692747a4
Victor Elias <victorgelias@gmail.com>
2023-12-19 17:36:34 -0300

    Revert "access-control: send jwt in gate request (#1047)"
    This reverts commit 2331013a1e4f6483f70c3db1527f23da19d8ded3.

--- handlers/accesscontrol/access-control.go
+++ handlers/accesscontrol/access-control.go
@@ -37,7 +37,6 @@ type PlaybackAccessControlRequest struct {
 	Pub       string `json:"pub"`
 	AccessKey string `json:"accessKey"`
 	Stream    string `json:"stream"`
-	Jwt       string `json:"jwt,omitempty"`
 }
 
 type GateAPICaller interface {
@@ -110,7 +109,6 @@ func (ac *AccessControlHandlersCollection) IsAuthorized(playbackID string, paylo
 			return false, nil
 		}
 		acReq.Pub = pub
-		acReq.Jwt = jwt
 
 		acReq.Type = "jwt"
 		cacheKey = "jwtPubKey_" + acReq.Pub
