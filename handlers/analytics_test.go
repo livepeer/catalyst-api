88843447b5dbb389fa72be67c5a71f69d121a5e7
Rafał Leszko <rafal@livepeer.org>
2024-03-05 16:19:08 +0100

    Fix unit tests

--- handlers/analytics_test.go
+++ handlers/analytics_test.go
@@ -18,7 +18,7 @@ type MockExternalDataFetcher struct {
 
 func (f *MockExternalDataFetcher) Fetch(playbackID string) (analytics.ExternalData, error) {
 	f.calledPlaybackIDs[playbackID] = true
-	return analytics.ExternalData{UserID: userID}, nil
+	return analytics.ExternalData{UserID: userID, SourceType: "stream"}, nil
 }
 
 type MockLogProcessor struct {
@@ -37,7 +37,7 @@ func TestHandleLog(t *testing.T) {
 		requestBody              string
 		wantHttpCode             int
 		wantExtFetchedPlaybackID string
-		wantProcessedLog         analytics.LogData
+		wantProcessedLogs        []analytics.LogData
 	}{
 		{
 			name: "valid payload",
@@ -51,32 +51,99 @@ func TestHandleLog(t *testing.T) {
 				"user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36",
 				"uid": "abcdef",
 				"events": [
-					{
+				   {
 						"type": "heartbeat",
 						"timestamp": 1234567895,
-						"errors": 2,
-						"playtime_ms": 4500,
-						"ttff_ms": 300,
-						"preload_time_ms": 1000,
-						"buffer_ms": 50
+						"errors": 0,
+						"autoplay_status": "autoplay",
+						"stalled_count": 5,
+						"waiting_count": 7,
+						"time_errored_ms": 18,
+						"time_stalled_ms": 20,
+						"time_playing_ms": 40,
+						"time_waiting_ms": 60,
+						"mount_to_play_ms": 80,
+						"mount_to_first_frame_ms": 100,
+						"play_to_first_frame_ms": 30,
+						"duration_ms": 40,
+						"offset_ms": 400,
+						"player_height_px": 123,
+						"player_width_px": 124,
+						"video_height_px": 12345,
+						"video_width_px": 124,
+						"window_height_px": 532,
+						"window_width_px": 234
 					},
 					{
 						"type": "error",
-						"timestamp": 1234567895
+						"timestamp": 1234567895,
+						"error_message": "error message",
+						"category": "offline"
 					}
 				]
 			}`,
 			wantHttpCode:             200,
 			wantExtFetchedPlaybackID: "123456",
-			wantProcessedLog: analytics.LogData{
-				SessionID:  "abcdef",
-				PlaybackID: "123456",
-				Browser:    "Chrome",
-				DeviceType: "desktop",
-				UserID:     userID,
-				PlaytimeMs: 4500,
-				BufferMs:   50,
-				Errors:     2,
+			wantProcessedLogs: []analytics.LogData{
+				{
+					SessionID:  "abcdef",
+					PlaybackID: "123456",
+					ViewerHash: "b5df095b93d1e63197392a5bc2d1f2de3dcc3170cedfe84d17ab29b116469cc0",
+					Protocol:   "video/mp4",
+					PageURL:    "https://www.fishtank.live/",
+					SourceURL:  "https://vod-cdn.lp-playback.studio/raw/jxf4iblf6wlsyor6526t4tcmtmqa/catalyst-vod-com/hls/362f9l7ekeoze518/1080p0.mp4?tkn=8b140ec6b404a",
+					Player:     "video-@livepeer/react@3.1.9",
+					UID:        "abcdef",
+					UserID:     userID,
+					Source:     "stream",
+					DeviceType: "desktop",
+					Browser:    "Chrome",
+					OS:         "macOS",
+					Data: analytics.LogDataEvent{
+						EventType:           "heartbeat",
+						EventTimestamp:      1234567895,
+						Errors:              0,
+						AutoplayStatus:      "autoplay",
+						StalledCount:        5,
+						WaitingCount:        7,
+						TimeErroredMS:       18,
+						TimeStalledMS:       20,
+						TimePlayingMS:       40,
+						TimeWaitingMS:       60,
+						MountToPlayMS:       80,
+						MountToFirstFrameMS: 100,
+						PlayToFirstFrameMS:  30,
+						DurationMS:          40,
+						OffsetMS:            400,
+						PlayerHeightPX:      123,
+						PlayerWidthPX:       124,
+						VideoHeightPX:       12345,
+						VideoWidthPX:        124,
+						WindowHeightPX:      532,
+						WindowWidthPX:       234,
+					},
+				},
+				{
+					SessionID:  "abcdef",
+					PlaybackID: "123456",
+					ViewerHash: "b5df095b93d1e63197392a5bc2d1f2de3dcc3170cedfe84d17ab29b116469cc0",
+					Protocol:   "video/mp4",
+					PageURL:    "https://www.fishtank.live/",
+					SourceURL:  "https://vod-cdn.lp-playback.studio/raw/jxf4iblf6wlsyor6526t4tcmtmqa/catalyst-vod-com/hls/362f9l7ekeoze518/1080p0.mp4?tkn=8b140ec6b404a",
+					Player:     "video-@livepeer/react@3.1.9",
+					UID:        "abcdef",
+					UserID:     userID,
+					Source:     "stream",
+					DeviceType: "desktop",
+					Browser:    "Chrome",
+					OS:         "macOS",
+					Data: analytics.LogDataEvent{
+						EventType:      "error",
+						EventTimestamp: 1234567895,
+						ErrorMessage:   "error message",
+						Category:       "offline",
+					},
+				},
 			},
 		},
 		{
@@ -154,7 +221,12 @@ func TestHandleLog(t *testing.T) {
 			if tt.wantHttpCode == http.StatusOK {
 				require.Equal(1, len(mockFetcher.calledPlaybackIDs))
 				require.True(mockFetcher.calledPlaybackIDs[tt.wantExtFetchedPlaybackID])
-				require.Equal(tt.wantProcessedLog, <-mockProcessor.processed)
+				for _, expLog := range tt.wantProcessedLogs {
+					processed := <-mockProcessor.processed
+					// Ignore timestamp
+					processed.ServerTimestamp = 0
+					require.Equal(expLog, processed)
+				}
 			} else {
 				require.Equal(0, len(mockFetcher.calledPlaybackIDs))
 			}
@@ -176,12 +248,14 @@ func TestParseAnalyticsGeo(t *testing.T) {
 			header: map[string][]string{
 				"X-Latitude":          {"50.06580"},
 				"X-Longitude":         {"19.94010"},
+				"X-City-Country-Code": {"PL"},
 				"X-City-Country-Name": {"Poland"},
 				"X-Region-Name":       {"Lesser Poland"},
 				"X-Time-Zone":         {"Europe/Warsaw"},
 			},
 			exp: AnalyticsGeo{
 				GeoHash:     "u2yhvdpyqj",
+				CountryCode: "PL",
 				Country:     "Poland",
 				Subdivision: "Lesser Poland",
 				Timezone:    "Europe/Warsaw",
@@ -193,6 +267,7 @@ func TestParseAnalyticsGeo(t *testing.T) {
 			header: map[string][]string{
 				"X-Latitude":          {"50.06580"},
 				"X-Longitude":         {"19.94010"},
+				"X-City-Country-Code": {"PL"},
 				"X-City-Country-Name": {"Poland"},
 			},
 			exp: AnalyticsGeo{
