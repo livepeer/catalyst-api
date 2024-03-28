16f83a33c6373581e2f6317e6f46c030c065a9a3
Max Holland <max@livepeer.org>
2024-03-28 11:47:06 +0000

    Don't generate thumbs for long recordings

--- pipeline/coordinator.go
+++ pipeline/coordinator.go
@@ -40,11 +40,10 @@ const (
 	// Execute the FFMPEG pipeline first and fallback to the external transcoding
 	// provider on errors.
 	StrategyFallbackExternal Strategy = "fallback_external"
-)
-
-const (
 	// Only mp4s of maxMP4OutDuration will have MP4s generated for each rendition
-	maxMP4OutDuration = 2 * time.Minute
+	maxMP4OutDuration          = 2 * time.Minute
+	maxRecordingMP4Duration    = 12 * time.Hour
+	maxRecordingThumbsDuration = maxRecordingMP4Duration
 )
 
 func (s Strategy) IsValid() bool {
@@ -384,8 +383,7 @@ func ShouldGenerateMP4(sourceURL, mp4TargetUrl *url.URL, fragMp4TargetUrl *url.U
 		return false
 	}
 	// We're currently memory-bound for generating MP4s above a certain file size
-	// This has been hitting us for long recordings, so do a crude "is it longer than 3 hours?" check and skip the MP4 if it is
-	const maxRecordingMP4Duration = 12 * time.Hour
+	// This has been hitting us for long recordings, so do a crude "is it longer than 12 hours?" check and skip the MP4 if it is
 	if clients.IsHLSInput(sourceURL) && durationSecs > maxRecordingMP4Duration.Seconds() {
 		return false
 	}
