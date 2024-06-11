316bd5f19ddc1576cef7542b3e4a87c3992d2e3f
gioelecerati <gioele@cerati.org>
2024-06-11 20:19:20 +0200

    Revert "pod-mon: re-enable to collect usage metrics"
    This reverts commit 6b1a1536e5422ed81a94e3c6d9deffbfb8a7b53a.

--- scripts/pod-mon.sh
+++ scripts/pod-mon.sh
@@ -22,57 +22,18 @@ log () {
   fi
 }
 
-# List of process names to monitor
-processes=("MistController" "MistProcLivepeer" "MistUtilLoad" "MistOutWebRTC" "MistInDTSC" "MistOutDTSC" "MistOutFLV" "MistInFLV" "MistInBuffer" "MistOutHTTPTS" "catalyst-api" "catalyst-uploader")
+# get cpu/mem usage
+top=$(top -b -n 1 -H -o %CPU | head -n 15)
+free=$(free -h)
+# get disk usage
+df=$(df -h)
+tmpusage=$(du -ch --max-depth=1 /tmp | sort -hr)
 
-# Function to calculate CPU and MEM usage for a specific process
-calculate_usage() {
-  local process_name=$1
-
-  # Get the output of ps aux filtered by the process name, skipping the header row and capturing only the CPU and MEM columns
-  ps aux | grep "$process_name" | grep -v grep | awk '{
-    cpu+=$3; mem+=$4; count+=1; if ($3 > max_cpu) max_cpu=$3
-  } END {
-    print cpu, mem, count, max_cpu
-  }'
-}
-
-# Get the number of CPU cores
-num_cores=$(grep -c ^processor /proc/cpuinfo)
-
-# Initialize the output as an empty string
-output="["
-
-# Loop through each process and calculate the usage
-for process in "${processes[@]}"; do
-  output_data=$(calculate_usage "$process")
-
-  cpu=$(echo "$output_data" | awk '{print $1}')
-  mem=$(echo "$output_data" | awk '{print $2}')
-  count=$(echo "$output_data" | awk '{print $3}')
-  max_cpu=$(echo "$output_data" | awk '{print $4}')
-
-  # Set defaults if count is not a number
-  if ! [[ "$count" =~ ^[0-9]+$ ]]; then
-    count=0
-  fi
-
-  if [ "$count" -gt 0 ]; then
-    avg_cpu_per_core=$(awk "BEGIN {print $cpu / $num_cores}")
-    avg_mem=$(awk "BEGIN {print $mem / $count}")
-  else
-    avg_cpu_per_core=0
-    avg_mem=0
-    max_cpu=0
-  fi
-
-  # Append the JSON-like string to the output string
-  output+="{\"process\": \"$process\", \"avg_cpu_per_core\": $avg_cpu_per_core, \"max_cpu_per_core\": $max_cpu, \"avg_mem_per_process\": $avg_mem}, "
-done
-
-# Remove the trailing comma and space, then close the JSON array
-output=${output%, }
-output+="]"
-
-# Print the final JSON string
-log "$output"
+log "System usage --------"
+log "$top"
+log "Mem usage --------"
+log "$free"
+log "Disk usage --------"
+log "$df"
+log "/tmp usage --------"
+log "$tmpusage"
