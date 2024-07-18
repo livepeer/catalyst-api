bc260ddac295f39b8d69c7d1ea79ba8ce869aa3f
Rafał Leszko <rafal@livepeer.org>
2024-07-18 16:44:49 +0200

    Switch serf communication from local to http calls
    We plan to run Serf member on a different host than catalyst-api, therefore it needs to communicate remotely rather than directly accessing Serf.

--- handlers/geolocation/geolocation.go
+++ handlers/geolocation/geolocation.go
@@ -67,16 +67,14 @@ func (l *streamPullRateLimit) mark(playbackID string) {
 
 type GeolocationHandlersCollection struct {
 	Balancer            balancer.Balancer
-	Cluster             cluster.Cluster
 	Config              config.Cli
 	Lapi                *api.Client
 	streamPullRateLimit *streamPullRateLimit
 }
 
-func NewGeolocationHandlersCollection(balancer balancer.Balancer, cluster cluster.Cluster, config config.Cli, lapi *api.Client) *GeolocationHandlersCollection {
+func NewGeolocationHandlersCollection(balancer balancer.Balancer, config config.Cli, lapi *api.Client) *GeolocationHandlersCollection {
 	return &GeolocationHandlersCollection{
 		Balancer:            balancer,
-		Cluster:             cluster,
 		Config:              config,
 		Lapi:                lapi,
 		streamPullRateLimit: newStreamPullRateLimit(streamSourceRetryInterval),
@@ -244,7 +242,7 @@ func (c *GeolocationHandlersCollection) resolveNodeURL(streamURL string) (string
 }
 
 func (c *GeolocationHandlersCollection) clusterMember(filter map[string]string, status, name string) (cluster.Member, error) {
-	members, err := c.Cluster.MembersFiltered(filter, "", name)
+	members, err := c.membersFiltered(filter, "", name)
 	if err != nil {
 		return cluster.Member{}, err
 	}
@@ -404,7 +402,7 @@ func (c *GeolocationHandlersCollection) getStreamPull(playbackID string, retryCo
 }
 
 func (c *GeolocationHandlersCollection) sendPlaybackRequestAsync(playbackID string, region string) {
-	members, err := c.Cluster.MembersFiltered(map[string]string{"region": region}, "", "")
+	members, err := c.membersFiltered(map[string]string{"region": region}, "", "")
 	if err != nil || len(members) == 0 {
 		glog.Errorf("Error fetching member list: %v", err)
 		return
@@ -420,6 +418,23 @@ func (c *GeolocationHandlersCollection) sendPlaybackRequestAsync(playbackID stri
 		}
 		resp.Body.Close()
 	}()
+}
+
+func (c *GeolocationHandlersCollection) membersFiltered(filter map[string]string, status, name string) ([]cluster.Member, error) {
+	resp, err := http.Get(c.Config.SerfMembersEndpoint)
+	if err != nil {
+		return []cluster.Member{}, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return []cluster.Member{}, fmt.Errorf("failed to get members: %s", resp.Status)
+	}
+	var members []cluster.Member
+	if err := json.NewDecoder(resp.Body).Decode(&members); err != nil {
+		return []cluster.Member{}, err
+	}
+
+	return cluster.FilterMembers(members, filter, status, name)
 }
 
 func parsePlus(plusString string) (string, string) {
